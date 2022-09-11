6ff4399407af5f0a5571d88f0536026b73ea0bc8
Aldanis Vigo <aldanisvigo@gmail.com>
2022-09-11 15:59:18 -0500

    Added comments to the code

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,27 @@
+//Main container for the game buttons
 const container = document.getElementById("whackachicken")
+
+//The winning score
 let winningScore = 8
+
+//The losing score
 let losingScore = 0
+
+//The score tracking variable
 let score = 0
+
+//Container where the score will be displayed
 const score_container = document.getElementById('score')
 
+//Array to store the chicken buttons
 let whackabuttons = []
+
+//Placeholder for the setInterval timer
 let timer = null;
 
+/*
+    Function to check for win or increase the score
+*/
 const increaseScore = () => {
     if(score == winningScore - 1){
         clearInterval(timer)
@@ -24,28 +39,54 @@ const increaseScore = () => {
     }
 }
 
+
+/*
+    Function to check for loss or decrease the score
+*/
 const decreaseScore = () => {
+    //If the score is less than or equal to the losing score
     if(score <= losingScore){
+        //Stop the timer
         clearInterval(timer)
+        //Ask the user if he/she wants to restart the game and let them know they lost
         let confirmRestart = confirm("You lost! Would you like to play again?")
+        
+        //If they choose to restart the game
         if(confirmRestart){
+            //Clear the score back to 0
             score = 0;
+            //And start the chicken randomizer again
             startChickenRandomizer()
         }else{
+            //Otherwise, just say goodbye
             alert("Alrighty. Goodbye burger!")
         }
     }else{
+        //Otherwise decrease the score
         score--
+
+        //Display the new score
         score_container.innerText = `Score : ${score}`
     }
 }
 
+
+/*
+    Function to create the 9 buttons
+*/
 const createButtons = () => {
+    //Iterate through the nine buttons
     for(let i = 0; i < 9; i++){
+        //Create the button element
         const btn = document.createElement('button')
+        //Add the css class name so we can target it with css
         btn.className = "whackabutton"
+        //And add an ID in case we need to access the button directly
         btn.id = "whackabutton_" + i
+        //Append the button to the button container div
         container.appendChild(btn)
+
+        //Add a click event listener to the button
         btn.onclick = (e) => {
             // Check if the chicken class is there
             let b = e.target;
@@ -57,22 +98,36 @@ const createButtons = () => {
                 decreaseScore()
             }
         }
+
+        //Push this button into the buttons array
         whackabuttons.push(btn)
     }
 }
 
-const startChickenRandomizer = () => {
-    const interval_time = 900
-    timer = setInterval(()=>{
-        
-        let selectedButton = Math.floor(Math.random() * 10)
-        for(let i = 0; i < 9; i++){
-            const s = document.getElementById('whackabutton_' + i)
-            if(i !== selectedButton){
-                //Take the chicken off this button
-                s.classList.remove(['chicken'])
 
-            }else{
+/*
+    Function starts a timer every 900ms and randomizes which button
+    the chicken will appear in
+*/
+const startChickenRandomizer = () => {
+    //The interval for switching the chicken
+    const interval_time = 900
+
+    //Set the timer to an interval function
+    timer = setInterval(()=>{
+        //Select a random button from 0-9
+        let selectedButton = Math.floor(Math.random() * 10)
+
+        //Iterate through all the available buttons
+        for(let i = 0; i < 9; i++){
+            //Get the current button
+            const s = document.getElementById('whackabutton_' + i)
+
+            //Check if the current button is the selected button
+            if(i !== selectedButton){ //If it's not the selected button
+                //Remove chicken off this button if it's there
+                s.classList.remove(['chicken'])
+            }else{ //Otherwise
                 //Add the chicken to this button
                 s.classList.add(['chicken'])
             }
@@ -81,9 +136,10 @@ const startChickenRandomizer = () => {
     },interval_time)
 }
 
-
-// createTitle()
+//Create all the chicken buttons
 createButtons()
+
+//Start the randomizer timer
 startChickenRandomizer()
 
 
